c6552845057abf880b490cda5e1632967547edbe
Aaliyah Merchant <aaliyah.merchant@kcl.ac.uk>
2023-09-23 18:21:25 +0100

    Completed primes exercise

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    curr = 2
+
+    while (len(list) < number_of_primes):
+        prime = True
+
+        for i in range(2, curr):
+            if (curr % i == 0):
+                prime = False
+                break
+        
+        if (prime):
+            list.append(curr)
+
+        curr += 1
+
     return list
